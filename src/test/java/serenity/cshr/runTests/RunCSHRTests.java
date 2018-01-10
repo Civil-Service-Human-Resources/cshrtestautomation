77cbecb72c20535e82f37f2c67de41df1289cfa5
ravigeda <ravi.geda@valtech.co.uk>
2018-01-10 15:21:29 +0000

    added datafile for api testing

--- src/test/java/serenity/cshr/runTests/RunCSHRTests.java
+++ src/test/java/serenity/cshr/runTests/RunCSHRTests.java
@@ -7,6 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {"src/test/resources/features"},
         glue = {"serenity.cshr.stepDefinitions"})
-
 public class RunCSHRTests {
+
+    //TODO
+    //Connect to database and fetch results
+    //Implement tests for working hours and contact details
+    //The pages are not scrolling to the bottom only partial screenshots are obtained- should they be unzoomed
+
 }
