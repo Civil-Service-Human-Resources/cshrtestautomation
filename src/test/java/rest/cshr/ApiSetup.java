647fe7c490a2fe94e26082668efca1e579b83abf
ravigeda <ravi.geda@valtech.co.uk>
2018-01-26 15:45:45 +0000

    "Modified automation for Api data setup"

--- src/test/java/rest/cshr/ApiSetup.java
+++ src/test/java/rest/cshr/ApiSetup.java
@@ -5,10 +5,10 @@ import io.restassured.RestAssured;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Before;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ApiSetup {
@@ -32,13 +32,42 @@ public class ApiSetup {
         List<JsonObject> excelRows = new ArrayList<>();
         for(int rows=0;rows<noOfRows-1;rows++){
             JsonObject dataConvert = new JsonObject();
+            String subHead = null;
+            JsonObject dept= new JsonObject() ;
             for (int i = 0; i < lastCell; i++) {
-                dataConvert.addProperty(sheetOne.getRow(0).getCell(i).toString(), sheetOne.getRow(rows + 1).getCell(i).toString());
+                if(sheetOne.getRow(0).getCell(i).toString().contains("/")){ //Splits heading and converts to
+                    List<String> headSplit = Arrays.asList(sheetOne.getRow(0).getCell(i).toString().split("/"));
+                    if(headSplit.get(1).equals("id")){
+                        dept.addProperty(headSplit.get(1), sheetOne.getRow(rows + 1).getCell(i).getNumericCellValue());
+                    }
+                    else {
+                        dept.addProperty(headSplit.get(1), sheetOne.getRow(rows + 1).getCell(i).toString());
+                    }
+                    if(subHead!=null){
+
+                    }
+                    else{
+                        subHead = headSplit.get(0);
+                    }
+                }
+                else{
+                    if(sheetOne.getRow(0).getCell(i).toString().contains("sal")||sheetOne.getRow(0).getCell(i).toString().contains("acancies")){
+                        dataConvert.addProperty(sheetOne.getRow(0).getCell(i).toString(), (int)sheetOne.getRow(rows + 1).getCell(i).getNumericCellValue());
+                    }else {
+                        dataConvert.addProperty(sheetOne.getRow(0).getCell(i).toString(), sheetOne.getRow(rows + 1).getCell(i).toString());
+                    }
+                }
+
             }
+            dataConvert.add(subHead,dept);
             excelRows.add(dataConvert);
         }
         fis.close();
         return excelRows;
     }
 
+    public static void main(String[] args) throws Exception{
+        ApiSetup as = new ApiSetup();
+        System.out.println(as.excelDataAsJson());
+    }
 }
--- src/test/java/rest/cshr/PostJobData.java
+++ src/test/java/rest/cshr/PostJobData.java
@@ -1,6 +1,5 @@
 package rest.cshr;
 
-import com.google.gson.JsonObject;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import net.serenitybdd.junit.runners.SerenityRunner;
@@ -9,7 +8,8 @@ import net.thucydides.core.annotations.Title;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SerenityRunner.class)
 public class PostJobData extends ApiSetup {
@@ -18,11 +18,15 @@ public class PostJobData extends ApiSetup {
     @Title("Post a valid job data with all fields matching requirement")
     @Test
     public void postJobData() throws Exception {
-        Response statuscode = SerenityRest.rest().given().contentType("application/json").body(excelDataAsJson().get(0)).when().post("/vacancy");
-        Assert.assertTrue(statuscode.body().asString().contains("probationary period are eligible to apply for"));
-        JsonPath evaluatorForJsonPath = statuscode.jsonPath();
-        String location = evaluatorForJsonPath.get("location");
-        Assert.assertEquals(location, excelDataAsJson().get(1).get("location").getAsString());
+        int excelRows = excelDataAsJson().size();
+        for(int i=0;i<excelRows;i++){
+            Response statuscode = SerenityRest.rest().given().contentType("application/json").body(excelDataAsJson().get(i)).when().post("/vacancy");
+            statuscode.getBody().prettyPrint();
+            Assert.assertTrue(statuscode.body().asString().contains("probationary period are eligible to apply for"));
+            JsonPath evaluatorForJsonPath = statuscode.jsonPath();
+            String location = evaluatorForJsonPath.get("location");
+            Assert.assertEquals(location, excelDataAsJson().get(i).get("location").getAsString());
+        }
     }
 
     @Title("Post an invalid job  with a missing mandatory field-location")
@@ -33,13 +37,15 @@ public class PostJobData extends ApiSetup {
 
     }
 
-   /* @Title("Post an invalid job with an extra field which doesn't exist in the schema and look for the error message returned")
+    @Title("Get valid data using location only")
     @Test
-    public void postInvalidJobDataWithExtraField() throws Exception{
-        JsonObject job = excelDataAsJson().get(1);
-        job.addProperty("someKey","someVlaue");
-        Response response = SerenityRest.rest().given().contentType("application/json").body(job).when().post("/vacancy");
-        Assert.assertEquals(200,response.getStatusCode());
+    public void getUsingLocation(){
+        /*LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);*/
+        Response response = SerenityRest.rest().given().contentType("application/json").get("http://localhost:8080/vacancy/search/location/london");
+        System.out.println(response.asString());
+        int sizeOfRes = response.getBody().jsonPath().get("content.size()");
+        List<Map<String,String>> jk = response.getBody().jsonPath().get("content");
+        Assert.assertEquals("london".toLowerCase(), jk.get(1).get("location").toLowerCase());
     }
-*/
 }
